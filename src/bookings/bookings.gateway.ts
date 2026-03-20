ecfef7a94603bd1ed68c2d01c4b526aac2f617a9
Jose Vargas <rafael.uruguay2022@gmail.com>
2026-03-19 22:36:24 -0300

    Update CORS configuration to use the correct production origin URL

--- src/bookings/bookings.gateway.ts
+++ src/bookings/bookings.gateway.ts
@@ -9,7 +9,7 @@ import { Server } from 'socket.io';
 @WebSocketGateway({
   cors: {
     origin: process.env.NODE_ENV === 'production' 
-      ? ['https://bigligasbeautybarberstudio.netlify.app']
+      ? ['https://bookingsjr.netlify.app']
       : true,
     credentials: true,
   },
